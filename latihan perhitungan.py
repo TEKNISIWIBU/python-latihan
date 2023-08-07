0ee5a80e3c52d0b19a79a4fe85b073f59e047748
TEKNISIWIBU <101981020+TEKNISIWIBU@users.noreply.github.com>
2023-08-07 13:45:52 +0700

    Update latihan perhitungan.py
    farenhait dan kelvin ke celcius dan reamur

--- latihan perhitungan.py
+++ latihan perhitungan.py
@@ -12,3 +12,17 @@ print("suhu adalah",farenhait,"F")
 #KELVIN
 kelvin = celcius+273
 print("suhu adalah",kelvin,"K")
+print("=========farent & kelvin to celcius==========")
+#farenhait to celcius
+faren = (5/9*(farenhait - 32))
+print("farenhait ke celcius",faren,"C")
+#kelvin to celcius
+kel = kelvin - 273
+print("kelvin ke celcius",kel,"C")
+print("=========farent & kelvin to reamur==========")
+#farenhait to celcius
+faren = 4/9*(farenhait - 32)
+print("farenhait ke celcius",faren,"C")
+#kelvin to celcius
+kel = 4/5*(kelvin - 273)
+print("kelvin ke celcius",kel,"C")
